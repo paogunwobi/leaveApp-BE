ba30e2c0c215d293cbf6ac558935f4637040eaec
Philip Ogunwobi <paogunwobi@gmail.com>
2021-04-05 09:08:08 +0100

    updates

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 require('dotenv').config();
 
+
 const server = require('./server');
+//testing api
+server.use("/", (req, res) => {
+    res.send("LeaveApp API works!!!");
+  });
 
 const PORT = process.env.PORT || 3300;
 
--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
-// const routes = require('../routes');
-// server.use('/api', routes);
 
 const server = express();
 server.use(express.json());
+
 
 //entry route for Staffs
 server.use("/api/v1/staff", require("../routes/staffRoute"));
